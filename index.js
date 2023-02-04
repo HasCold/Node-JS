ce005d821e1f52a3a7d7557398f46bc3f1ade035
Hasan <ha03330224926@gmail.com>
2023-02-04 20:42:03 +0500

    Second commit

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Hasan");
+
+// The node:console module provides a simple debugging console that is similar to the JavaScript console mechanism provided by web browsers.
+
+// That's why the web-browser have different console and node have different console  
