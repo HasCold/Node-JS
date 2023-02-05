7bc84e9df54384ab9aaa7953d4e643e4c812384e
Hasan <ha03330224926@gmail.com>
2023-02-05 22:34:25 +0500

    Add Nodemon-In-Node.js branch

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-console.log("Hasan");
+// Nodemon package continuosly run the node.js ; you haven't run the node over and over again
+// whenever you need to install nodemon package :- npm i nodemon -g (-g shows globally installed nodemon on your system)
+// After you have installed the nodemon , run "nodemon ." for continuously run the Node.js
+// To exit nodemon, simply press CTRL + C in your terminal. 
+// Node js is async
 
-// The node:console module provides a simple debugging console that is similar to the JavaScript console mechanism provided by web browsers.
-
-// That's why the web-browser have different console and node have different console  
+console.log("Try Nodemon with Node js...")
