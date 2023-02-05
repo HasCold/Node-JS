5d7b0e7bdf6d26377d9a7d1371c1b50cf0f038ca
Hasan <ha03330224926@gmail.com>
2023-02-05 21:35:25 +0500

    Add Package.json branch

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const http = require("http");
+// what is package.json?
+// package.json file holds a detail reagrding your coding like project versions, dependency, how much packages and commands
+// package-lock.json file manages the information related to your packages
+// To create a package.json file :- npm init 
+// Node.JS is a single threaded not a multithreaded -->> Interveiw Question
+// If you deleted node_modules folder mistakenly then simply run the command "npm install"
+// We shouldn't push the node_modules folder to the git bc of storage size 
 
-// What HTTP core module did (Interview Question)
-// HTTP module handle the server request and repsonse in the Node.js
-
-http.createServer((req, res) => {
-   res.write("<h1>Hi, This is Hasan</h1>");
-   res.end();
-}).listen(4500);
+const colors = require("colors");
+console.log("Package.json".zebra);
