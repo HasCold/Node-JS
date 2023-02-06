92e482945d9b0658daa4b02685faf815f3d54032
Hasan <ha03330224926@gmail.com>
2023-02-06 20:26:27 +0500

    Add cmd-Line-input branch

--- data.js
+++
@@ -1,8 +0,0 @@
-//  *********************** Static Data used in API **************************
-const data =[
-    {name: "HasCold", email: "mha@test", contact: "9999"},
-    {name: "Sam", email: "sam@test"},
-    {name: "Peter", email: "peter@test"},
-]
-
-module.exports = data;
--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const http = require("http");  // http module to make an API for server request and response
-const data = require("./data");
+// process is a very large property 
+// process.argv -->> From this we can take input from command line
+console.log(process.argv) // argv -->> argument vector 
 
-http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type" : "application\json"});
-    res.write(JSON.stringify(data));
-    res.end();
-}).listen(5000);
+// We can give input by cmd line -->> node . hello.txt "Hi, this is me"
+
+const input = process.argv
+// In file system : - fs.writeFileSync("file name", "file content");
+// const fs = require("fs");
+// fs.writeFileSync(input[2], input[3]);
+
+// Add OR Remove the file
+
+const gs = require("fs");
+if(input[2]==="add"){
+    gs.writeFileSync(input[3], input[4])
+}else if(input[2]==="remove"){  // Remove File
+    gs.unlinkSync(input[3]);
+}else{
+    console.log("Invalid Output");
+}
