98faa144d23d68c574fadc1b8dd513d199800a17
Hasan <ha03330224926@gmail.com>
2023-02-07 20:48:49 +0500

    Modify Branch

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Detailed Explanation in the Manual
 console.log("Starting Up ...");
 
 setTimeout(() => {   // setTimeout() is a part of C++ and related to the Node APIs and Callback Queue ; setTimeout() is a inherit library of C++ that's why the function goes to the Node APIs (Means two programming languages interface) and also similar to this other library which we are using in node.js that will go into the Node API's block and CallBack Queue block ; 
