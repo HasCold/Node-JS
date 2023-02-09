9199d7cb36827d8d20cdc18ea0618e12fc94fb37
Hasan <ha03330224926@gmail.com>
2023-02-09 21:50:14 +0500

    Add Template-Engine_ejs branch

--- index.js
+++ index.js
@@ -2,20 +2,31 @@ const express = require("express");
 const path = require("path");
 // path :-- By path module we can access the folder of our project
 
-
 const app = express(); // app Means it is executed function of express ; express has another static function which we used below
 const publicPath = path.join(__dirname, "public");
 
-// app.use(express.static(publicPath));
-// What is static method :-- static method load the static content of our pages
+// These parameters are fixed 
+app.set("view engine", "ejs"); // make a folder of views which is the By-Default configuration because we are using the template engine
 
 app.get("", (_, res) => {   // instead of req we use _ for short 
     res.sendFile(`${publicPath}/index.html`)
 });
+
+// we get dynamic data
+
+app.get("/profile", (_, res) => {   // instead of req we use _ for short 
+    const user = {
+        name : "Hasan",
+        email : "ha03@gmail.com",
+        city : "Karachi",
+    }
+    res.render("profile", {user});
+});
+
 app.get("/aboutme", (_, res) => {   // instead of req we use _ for short 
     res.sendFile(`${publicPath}/about.html`)
 });
-app.get("/help", (_, res) => {   // instead of req we use _ for short 
+app.get("/contact", (_, res) => {   // instead of req we use _ for short 
     res.sendFile(`${publicPath}/help.html`)
 })
 
