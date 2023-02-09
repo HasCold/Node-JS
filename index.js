3bdb084780d5166081589cd3427de6b448e5bc4c
Hasan <ha03330224926@gmail.com>
2023-02-09 23:20:06 +0500

    Add Dynamic-Page_In-Node.js branch

--- index.js
+++ index.js
@@ -19,10 +19,15 @@ app.get("/profile", (_, res) => {   // instead of req we use _ for short
         name : "Hasan",
         email : "ha03@gmail.com",
         city : "Karachi",
-        age : "20",
+        age : "20", 
+        skills : ["JS", "Node.js", "React.js", "C++"]
     }
     res.render("profile", {user});
 });
+
+app.get("/login", (_, res) => {
+    res.render("login");
+})
 
 app.get("/aboutme", (_, res) => {   // instead of req we use _ for short 
     res.sendFile(`${publicPath}/about.html`)
