450e9e3c41b2ca2fd653a7cfad0bdeac75c4ce96
Hasan <ha03330224926@gmail.com>
2023-02-12 17:20:14 +0500

    Add CRUD_Opeartions_IN_MongoDB branch

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
-// Middleware :- Apply middleware only with routes
-// We can access the request and response by middleware and also modify them 
+//   npm i mongodb  -->> The official Mongodb driver for Node.js
 
-// Route-Level Middleware :- Implement on a single route, group of route, all over the routes of application;
+// Instead of using localhost we use 127.0.0.1
+// The reason for this is that "localhost" and "127.0.0.1" both refer to the same IP address, which is the loopback address on the local machine.
 
-const express = require("express");
-const path = require("path"); // path :-- By path module we can access the folder of our project
-const publicPath = path.join(__dirname, "public");
-const app = express(); // app Means it is executed function of express ; express has another static function which we used below
-const reqFilter = require("./middleware")
+// In IP networking, "localhost" is a hostname that resolves to the IP address 127.0.0.1, which is used to access the network services running on the same machine. When a client program, such as a web browser or Node.js application, wants to access a network service running on the same machine, it typically connects to "localhost" or "127.0.0.1".
 
-// Below we apply middleware on a single route
-app.get("/login",reqFilter ,(_, res) => {  // Here we add the Route-Level Middleware on the login page
-    res.send("<h1> login Page </h1>");
-})
+// In the context of MongoDB, using "localhost" or "127.0.0.1" in the connection URL tells the MongoDB client to connect to the MongoDB server running on the same machine. By using "127.0.0.1" instead of "localhost", you are specifying the IP address directly, which can sometimes be more reliable and efficient than using the hostname.
 
-app.get("/", (_, res) => {
-    res.sendFile(`${publicPath}/index.html`)
-});
+// localhost is a special term that refers to the current machine, so it's essentially a shortcut for 127.0.0.1
+// 127.0.0.1 is the IP address of the localhost, which is the same machine you're running your Node.js application on.
+// By replacing localhost with 127.0.0.1, you are explicitly specifying the IP address of the machine you want to connect to, instead of using the shortcut. This can sometimes help resolve connection issues because it's a more direct approach.
 
-// Below we apply middleware on a single route
-app.get("/about",reqFilter, (_, res) => {  // Here we add the Route-Level Middleware on the about page    
-    res.sendFile(`${publicPath}/about.html`)
-});
+const {MongoClient} = require("mongodb");
+const url = "mongodb://127.0.0.1:27017";   // Path to tell the node location of mongodb
+const client = new MongoClient(url);
+const database = "e-comm";
 
-app.get("/contact", (_, res) => {   // instead of req we use _ for short 
-    res.sendFile(`${publicPath}/help.html`)
-})
+async function getData(){
+    let result = await client.connect();  // client is a promise  ; if we put await the promise will handle by themself; 
+    console.log("Connected Successfully to server") 
+    let db = result.db(database);
+    let collection = db.collection("products");
+    let res = await collection.find({}).toArray();
+    console.log(res);
+    return "done";
+}
 
-// How to laod the file in the get Method ; Interview Question 
-app.get("*", (_, res) => {   // * -->> we use this so by-Defalut the wrong URL page will be rendered the no-page.html 
-    res.sendFile(`${publicPath}/noPage.html`)
-})
-app.listen(5000);
-
-//        TYPES OF MIDDLEWARE
-
-// Application-level middleware
-// Router-level middleware
-// Error-Handling middleware
-// Built-In middleware
-// Third-Party middleware
+getData();
