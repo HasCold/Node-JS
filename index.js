b3aa0db92cdf5cd01e9d77c6e4aaab4248fc7ae0
Hasan <ha03330224926@gmail.com>
2023-02-16 19:46:59 +0500

    Add GET-DELETE-PUT_API branch

--- index.js
+++ index.js
@@ -11,4 +11,33 @@ app.post("/create", async (req, res) => {
     console.log(result); 
     res.send(result);
 })
+
+app.get("/list", async (req, res) => {
+    let data = await Product.find();
+    res.send(data);    
+})
+
+app.delete("/delete/:_id", async (req, res) => {
+    console.log(req.params);
+    let data = await Product.deleteOne(req.params);
+    res.send(data);    
+})
+
+app.put("/update/:_id", async (req, res) => {
+    console.log(req.params);
+    let data = await Product.updateOne(
+        req.params,  // -->> condition
+        { 
+            $set : req.body
+        },  
+        );
+    res.send(data);    
+})
+
 app.listen(5000);
+
+// Interview Question : - 
+
+// Q. Does id put in the body or query params ??
+// A. In the DELETE method we put id directly into the body ; 
+// But in the PUT method , it is upto you whether you have to put id  in the query params or body
