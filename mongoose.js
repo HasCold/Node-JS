4f0dded25cb091781c0fda91b72920c5146bbd6a
Hasan <ha03330224926@gmail.com>
2023-02-16 13:17:20 +0500

    Add Node.js-Mongoose branch

---
+++ mongoose.js
@@ -0,0 +1,6 @@
+// Mongoose is a advanced npm package that connects MongoDb with the Node.js
+
+// Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment. Mongoose supports Node.js and Deno (alpha).
+// In Mongoose, we can apply schemas , validation and control the field
+// npm i mongoose
+
