d943e733dc15bb8cff908f9743fd5ca02effba11
Hasan <ha03330224926@gmail.com>
2023-02-16 13:16:34 +0500

    Add Node.js-Mongoose branch

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const dbConnect = require("./mongodb")
+const mongoose = require("mongoose");
 
-// BELOW ARE THE TWO ASYNCHRONOUS OPEARATION USING .then method and async await ; But the most preferable method is async await  
-// 1. -->>
-// dbConnect().then((res) => {
-//     res.find({}).toArray().then((data) => {   // It will give you a promises
-//         console.log(data);
-//     })
-// })
-
-// 2. -->> 
 const main = async () => {
-    let data = await dbConnect();
-    data = await data.find({}).toArray();  // Read data from database
-    console.log(data);
-}
+    await mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
+    
+    // Schema :- a schema is a way to define the structure and properties of your data ; every field in the database is define into the schemas
+    // Model : - A model uses the schemas and connect the mongoDb and Node.js
 
+    const ProductSchemas = new mongoose.Schema({
+        name : String,
+        price : Number
+    });
+    // Create a model for that schema -->> mongoose.model('User', userSchema)
+    const ProductModel = mongoose.model("products", ProductSchemas);
+    let data = new ProductModel({name : "m-10", price : 1200});  // new instance
+    let result = await data.save();  
+    console.log(result);
+}    
 main();
