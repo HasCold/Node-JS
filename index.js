61424fb335c25153989f12fc349ff913cd533a4a
Hasan <ha03330224926@gmail.com>
2023-02-17 12:04:31 +0500

    Add Search_API-In-Node.js-With-Mongoose branch

--- index.js
+++ index.js
@@ -5,33 +5,18 @@ const Product = require("./product");
 const app = express();
 app.use(express.json());
 
-app.post("/create", async (req, res) => {
-    let data = new Product(req.body);
-    let result = await data.save();
-    console.log(result); 
-    res.send(result);
-})
-
-app.get("/list", async (req, res) => {
-    let data = await Product.find();
-    res.send(data);    
-})
-
-app.delete("/delete/:_id", async (req, res) => {
-    console.log(req.params);
-    let data = await Product.deleteOne(req.params);
-    res.send(data);    
-})
-
-app.put("/update/:_id", async (req, res) => {
-    console.log(req.params);
-    let data = await Product.updateOne(
-        req.params,  // -->> condition
-        { 
-            $set : req.body
-        },  
-        );
-    res.send(data);    
+app.get("/search/:key", async (req, res) => {  // -->> FIND OR READ method
+    console.log(req.params.key);
+    let data = await Product.find(
+        {
+            "$or" : [
+                {"name" : {$regex : req.params.key}},
+                {"brand" : {$regex : req.params.key}},
+                {"category": {$regex : req.params.key}}
+            ]
+        }
+    );
+    res.send(data);
 })
 
 app.listen(5000);
